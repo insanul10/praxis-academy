bc12aa77096a78498700064ad2df37431de1e444
insanul10 <insanul10@gmail.com>
2019-09-26 10:57:30 +0700

    Pertemuan ke-15

---
+++ novice/03-05/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import notif from '@/components/notif.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
